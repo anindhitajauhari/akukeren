81091b7d14bc6d1dcc9ea176a09b1c430367b591
unknown <andi.jauhari@student.president.ac.id>
2024-01-10 14:35:26 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Price List</title>
+    <style>
+        table {
+            width: 80%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #ffc0cb;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h2>Pink Themed Cake Price List</h2>
+
+    <table>
+        <tr>
+            <th>Cake Type</th>
+            <th>Size</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>Pink Velvet Cake</td>
+            <td>8-inch</td>
+            <td>$30</td>
+        </tr>
+        <tr>
+            <td>Strawberry Delight</td>
+            <td>10-inch</td>
+            <td>$45</td>
+        </tr>
+        <tr>
+            <td>Raspberry Swirl</td>
+            <td>6-inch</td>
+            <td>$25</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
